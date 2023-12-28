f67cc195fb5331e875721cb460912e01cb7ac8be
Aleksey Burov <burovalexprogrammer@gmail.com>
2023-12-28 20:59:08 +0200

    l33

--- POB2312/Program.cs
+++ POB2312/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ваше имя:");
+string name = Console.ReadLine();
+Console.WriteLine("Меня зовут, " + name);
