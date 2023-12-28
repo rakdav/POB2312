d02aafec12d98209bfbb2321984d7e9cf7e3fea7
Aleksey Burov <burovalexprogrammer@gmail.com>
2023-12-28 20:56:20 +0200

    l32

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+double b = double.Parse(Console.ReadLine());
+double s = a + b;
+
+//Console.WriteLine(a + "+" + b + "=" + s);
+//Console.WriteLine("Cумма равна:" + s);
+
+Console.WriteLine($"{a}+{b}={s}");
+Console.WriteLine($"Cумма равна:{s}");
+
+//Console.WriteLine("{0}+{1}={2}", a, b, s);
+//Console.WriteLine("Cумма равна:{0}", s);
--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+//Console.Write("Введите число:");
+//int n = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Вы ввели число {n}");
+//Console.WriteLine($"{1} {13} {49}");
+//Console.WriteLine("7  15  100");
+Console.Write("Введите три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}  {b}  {c}");
+
+
