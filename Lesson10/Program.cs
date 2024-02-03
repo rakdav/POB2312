cfa2af29e23d928712b2c9cba19b0867d564727e
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-02-03 19:51:52 +0200

    Lesson10

---
+++ Lesson10/Program.cs
@@ -0,0 +1,51 @@
+﻿//Console.Title = "Тема 10";
+//Console.Write("Введите первое число:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Введите второе число:");
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine(Math.Max(x,y));
+//Console.WriteLine($"|{x}|={Math.Abs(x)} |{y}|={Math.Abs(y)}");
+//Console.WriteLine(Math.Floor(x));
+//Console.WriteLine($"{Math.Sqrt(x):F2}");
+//Console.WriteLine(Math.Ceiling(x));
+//Console.WriteLine(Math.Pow(x,y));
+//Console.WriteLine(Math.Round(x));
+//Console.WriteLine(Math.DivRem((int)x,(int)y));
+//Console.WriteLine($"{Math.Sqrt(x*x+y*y):F2}");
+
+//1
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//double y = (x >= 0) ? x : -x;
+//Console.WriteLine($"|{x}|={y}");
+
+//2
+//Console.Write("Введите x:");
+//int x = int.Parse(Console.ReadLine());
+//string res = (x % 2 == 0) ? "Четное" : "Нечетное";
+//Console.WriteLine(res);
+
+//3
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+
+//if(x>0) Console.WriteLine("Положительное");
+//else if(x<0) Console.WriteLine("Отрицательное");
+//     else Console.WriteLine("Равно нулю");
+
+//Console.WriteLine((x>0)? "Положительное":(x<0)? "Отрицательное": "Равно нулю");
+
+//4
+using System.Threading.Channels;
+
+Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите x:");
+double y = double.Parse(Console.ReadLine());
+if(x>y) Console.WriteLine($"наибольшее {x}");
+else if(x<y) Console.WriteLine($"наибольшее {y}");
+     else Console.WriteLine("равны");
+
+string res=(x>y)?$"наибольшее {x}":
+    (x<y)?$"наибольшее {y}":"равны";
+Console.WriteLine(res);
---
+++ Lesson9/Program.cs
@@ -0,0 +1,22 @@
+﻿//byte a = 254;
+//Console.WriteLine(Convert.ToString(a, 2));
+
+//Console.WriteLine(Convert.ToString(~a, 2));
+//Console.WriteLine(16>>3);
+//int x = 1, y = -1;
+//Console.WriteLine(x*x+y*y<=4);
+//x = 1;y = 2;
+//Console.WriteLine((x>=0)||(y*y!=4));
+//Console.Write("Введите N:");
+//int N = int.Parse(Console.ReadLine());
+//Console.WriteLine(N%5==0||N%7==0);
+//Console.WriteLine(N%4==0&&N%10!=0);
+Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+if ((x >= 2) && (y >= 1 || y <= -1.5))
+    Console.WriteLine("Попал");
+else
+    Console.WriteLine("Не попал");
+
