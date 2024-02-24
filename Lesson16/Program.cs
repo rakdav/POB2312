eed7cd49f5a29bc9876abea3e3450266643be978
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-02-24 21:21:21 +0200

    lesson16Last

--- Lesson16/Program.cs
+++ Lesson16/Program.cs
@@ -70,36 +70,108 @@
 //Создайте массив из 8 строк и найдите в нем самую короткую и 
 //самую длинную строку, а также выведите на экран все строки,
 //содержащие букву "а". 
-string[] mas = new string[8];
+//string[] mas = new string[8];
+//Random random = new Random();
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    string temp = "";
+//    for(int j = 1; j < random.Next(1,30); j++)
+//    {
+//        temp += (char)random.Next(97, 123);
+//    }
+//    mas[i] = temp;
+//    Console.Write(mas[i]+" ");
+//}
+//Console.WriteLine();
+//int max = mas[0].Length, min = mas[0].Length;
+//string minStr = mas[0], maxStr = mas[0];
+//for (int i = 1; i < mas.Length; i++)
+//{
+//    if (mas[i].Length > max)
+//    {
+//        max = mas[i].Length;
+//        maxStr = mas[i];
+//    }
+//    if (mas[i].Length < min)
+//    {
+//        min = mas[i].Length;
+//        minStr = mas[i];
+//    }
+//    if (mas[i].Contains("a")) Console.Write(mas[i]+" ");
+//}
+//Console.WriteLine();
+//Console.WriteLine("max="+maxStr);
+//Console.WriteLine("min="+minStr);
+
+//Сортировка массива
+//Random random = new Random();
+//int[] mas = new int[random.Next(20)];
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    mas[i] = random.Next(1,100);
+//    Console.Write(mas[i]+" ");
+//}
+//for (int i = 0; i < mas.Length-1; i++)
+//{
+//    for (int j = i+1; j < mas.Length; j++)
+//    {
+//        if (mas[i] > mas[j])
+//        {
+//            int temp = mas[i];
+//            mas[i] = mas[j];
+//            mas[j] = temp;
+//        }
+//    }
+//}
+//Console.WriteLine();
+//foreach (int i in mas)
+//{
+//    Console.Write(i+" ");
+//}
+//Console.WriteLine();
+
+//Создайте массив из 30 случайных целых чисел и выведите на экран 
+//все элементы, которые встречаются в массиве больше одного раза.
+
+int[] mas = new int[30];
 Random random = new Random();
 for (int i = 0; i < mas.Length; i++)
 {
-    string temp = "";
-    for(int j = 1; j < random.Next(1,30); j++)
-    {
-        temp += (char)random.Next(97, 123);
-    }
-    mas[i] = temp;
+    mas[i] = random.Next(20);
     Console.Write(mas[i]+" ");
 }
 Console.WriteLine();
-int max = mas[0].Length, min = mas[0].Length;
-string minStr = mas[0], maxStr = mas[0];
-for (int i = 1; i < mas.Length; i++)
+Array.Sort(mas);
+foreach (int item in mas)
 {
-    if (mas[i].Length > max)
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+int count = 0;
+int temp=-1;
+for(int i = 1; i < mas.Length; i++)
+{
+    if (mas[i] == mas[i - 1])
     {
-        max = mas[i].Length;
-        maxStr = mas[i];
+        temp = mas[i];
+        count++;
     }
-    if (mas[i].Length < min)
+    else if(mas[i] != mas[i - 1])
     {
-        min = mas[i].Length;
-        minStr = mas[i];
+        if (count > 0)
+        {
+            Console.Write(temp + " ");
+            count = 0;
+        }
+    }
+    if (i == mas.Length - 1)
+    {
+        if (count > 0)
+        {
+            Console.Write(temp + " ");
+        }
     }
 }
-Console.WriteLine("max="+maxStr);
-Console.WriteLine("min="+minStr);
 
 
 
