deefb63b8b0165a32295990817c2e869fe4a4286
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-03-07 20:52:21 +0200

    Lesson19-1

--- Lesson18/Program.cs
+++ Lesson18/Program.cs
@@ -143,48 +143,78 @@
 //    Console.WriteLine();
 //}
 
-string[,] mas ={
-    {"","","x","x","",""},    
-    {"","x","","","x",""},
-    {"x","","","","","x"},    
-    {"","x","x","x","x",""},
-    {"","","x","x","",""},    
-    {"","","x","x","",""},
-    {"","","x","x","",""},    
-    {"","","x","x","",""},
-    {"","","x","x","",""},   
-    {"","","x","x","",""},
-    {"","x","x","x","x",""}};
-string answer = "no"; bool isChecked = false;
+//string[,] mas ={
+//    {"","","x","x","",""},    
+//    {"","x","","","x",""},
+//    {"x","","","","","x"},    
+//    {"","x","x","x","x",""},
+//    {"","","x","x","",""},    
+//    {"","","x","x","",""},
+//    {"","","x","x","",""},    
+//    {"","","x","x","",""},
+//    {"","","x","x","",""},   
+//    {"","","x","x","",""},
+//    {"","x","x","x","x",""}};
+//string answer = "no"; bool isChecked = false;
+//do
+//{
+//    Console.Clear(); 
+//    for (int j = 0; j < mas.GetUpperBound(0) + 1; j++)
+//    {
+//        for (int l = 0; l < mas.GetUpperBound(1) + 1; l++)
+//        {
+//            if (mas[j, l] != null)
+//            {
+//                Console.Write($"{mas[j, l],2}");
+//            }
+//        }
+//        Console.WriteLine();
+//    }
+//    if (isChecked == false) Console.Write("Включить лампочку?");
+//    else Console.Write("Выключить лампочку?");
+//    answer = Console.ReadLine()!; 
+//    if (answer == "yes") isChecked = !isChecked;
+//    if (isChecked)
+//    {
+//        mas[1, 2] = "x"; mas[1, 3] = "x";
+//        mas[2, 2] = "x"; mas[2, 3] = "x";
+//        mas[2, 1] = "x"; mas[2, 4] = "x";
+//    }
+//    else
+//    {
+//        mas[1, 2] = ""; mas[1, 3] = "";
+//        mas[2, 2] = ""; mas[2, 3] = "";
+//        mas[2, 1] = ""; mas[2, 4] = "";
+//    }
+//}
+//while (true);
+Random random = new Random();
+double[,] mas = new double[3, 3];
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+	for (int j = 0; j < mas.GetLength(1); j++)
+	{
+		mas[i, j] = random.NextDouble() * 20;
+		Console.Write($"{mas[i,j]:F2} ");
+	}
+	Console.WriteLine();
+}
+int n, m;
+Console.Write($"Введите номер строки <= {mas.GetLength(1)}:");
 do
 {
-    Console.Clear(); 
-    for (int j = 0; j < mas.GetUpperBound(0) + 1; j++)
-    {
-        for (int l = 0; l < mas.GetUpperBound(1) + 1; l++)
-        {
-            if (mas[j, l] != null)
-            {
-                Console.Write($"{mas[j, l],2}");
-            }
-        }
-        Console.WriteLine();
-    }
-    if (isChecked == false) Console.Write("Включить лампочку?");
-    else Console.Write("Выключить лампочку?");
-    answer = Console.ReadLine()!; 
-    if (answer == "yes") isChecked = !isChecked;
-    if (isChecked)
-    {
-        mas[1, 2] = "x"; mas[1, 3] = "x";
-        mas[2, 2] = "x"; mas[2, 3] = "x";
-        mas[2, 1] = "x"; mas[2, 4] = "x";
-    }
-    else
-    {
-        mas[1, 2] = ""; mas[1, 3] = "";
-        mas[2, 2] = ""; mas[2, 3] = "";
-        mas[2, 1] = ""; mas[2, 4] = "";
-    }
+	n = int.Parse(Console.ReadLine());
+   if(n > mas.GetLength(0)) Console.Write($"Введите номер строки <= {mas.GetLength(1)}:");
 }
-while (true);
+while (n > mas.GetLength(1));
+m = int.Parse(Console.ReadLine());
+double val = double.Parse(Console.ReadLine());
+mas[n-1, m-1] = val;
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        Console.Write($"{mas[i, j]:F2} ");
+    }
+    Console.WriteLine();
+}
---
+++ Lesson19/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Write("Введите максимальное количество задач:");
+int n = int.Parse(Console.ReadLine());
+string[,] mas = new string[n, 4];
+int count = 0;//количество задач
+int number;
+do
+{
+    Console.Clear();
+    Console.WriteLine("Меню:");
+    Console.WriteLine("1. Добавить задачу\n" +
+        "2. Удалить задачу\n" +
+        "3. Перезаписать задачу\n" +
+        "4. Вывести список задач\n" +
+        "5. Выход");
+    Console.Write("Введите действие:");
+    number = int.Parse(Console.ReadLine());
+    switch (number)
+    {
+        case 1:
+            {
+                if (count < n)
+                {
+                    Console.Write("Введите название задачи:");
+                    string name = Console.ReadLine();
+                    Console.Write("Введите дату задачи:");
+                    string date = Console.ReadLine();
+                    Console.Write("Введите время задачи:");
+                    string time = Console.ReadLine();
+                    Console.Write("Введите приоритет задачи:");
+                    string priority = Console.ReadLine();
+                    mas[count, 0] = name;
+                    mas[count, 1] = date;
+                    mas[count, 2] = time;
+                    mas[count, 3] = priority;
+                    count++;
+                }
+                else Console.WriteLine("Нет свободных мест");
+            }
+            break;
+        case 2:break;
+        case 3:break;
+        case 4:
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    for (int j = 0; j <mas.GetLength(1); j++)
+                    {
+                        Console.Write(mas[i,j]+" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            break;
+        case 5:break;
+    }
+    Console.Write("Введите действие:");
+    number = int.Parse(Console.ReadLine());
+}
+while (true);
