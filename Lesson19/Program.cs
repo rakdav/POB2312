30b7e9baffe8b5cde16ec7e1a968dcfab87aa49c
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-03-09 20:07:58 +0200

    Lesson20-1

--- Lesson19/Program.cs
+++ Lesson19/Program.cs
@@ -1,104 +1,118 @@
-﻿//Console.Write("Введите максимальное количество задач:");
-//int n = int.Parse(Console.ReadLine());
-//string[,] mas = new string[n, 4];
-//int count = 0;//количество задач
-//int number;
-//bool start = true;
-//do
-//{
-//    Console.Clear();
-//    Console.WriteLine($"Всего задач {n}, свободных {n-count}");
-//    Console.WriteLine("Меню:");
-//    Console.WriteLine("1. Добавить задачу\n" +
-//        "2. Удалить задачу\n" +
-//        "3. Перезаписать задачу\n" +
-//        "4. Вывести список задач\n" +
-//        "5. Выход");
-//    try
-//    {
-//        Console.Write("Введите действие:");
-//        number = int.Parse(Console.ReadLine());
+﻿Console.Write("Введите максимальное количество задач:");
+int n = int.Parse(Console.ReadLine());
+string[,] mas = new string[n, 4];
+int count = 0;//количество задач
+int number;
+bool start = true;
+string search = "";
+do
+{
+    Console.Clear();
+    Console.WriteLine($"Всего задач {n}, свободных {n - count}");
+    Console.WriteLine("Меню:");
+    Console.WriteLine("1. Добавить задачу\n" +
+        "2. Удалить задачу\n" +
+        "3. Перезаписать задачу\n" +
+        "4. Поиск\n" +
+        "5. Выход");
+    if (search != "")
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (mas[i,0].StartsWith(search))
+            Console.WriteLine($"{i + 1,-3}{mas[i, 0],-20}{mas[i, 1],-10}" +
+                $"{mas[i, 2],-10}{mas[i, 3],5}");
+        }
+    }
+    else
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"{i + 1,-3}{mas[i, 0],-20}{mas[i, 1],-10}" +
+                $"{mas[i, 2],-10}{mas[i, 3],5}");
+        }
+    }
+    try
+    {
+        Console.Write("Введите действие:");
+        number = int.Parse(Console.ReadLine());
 
-//        switch (number)
-//        {
-//            case 1:
-//                {
-//                    if (count < n)
-//                    {
-//                        Console.Write("Введите название задачи:");
-//                        string name = Console.ReadLine();
-//                        Console.Write("Введите дату задачи:");
-//                        string date = Console.ReadLine();
-//                        Console.Write("Введите время задачи:");
-//                        string time = Console.ReadLine();
-//                        Console.Write("Введите приоритет задачи:");
-//                        string priority = Console.ReadLine();
-//                        mas[count, 0] = name;
-//                        mas[count, 1] = date;
-//                        mas[count, 2] = time;
-//                        mas[count, 3] = priority;
-//                        count++;
-//                    }
-//                    else
-//                    {
-//                        Console.WriteLine("Нет свободных мест");
-//                        Console.ReadKey();
-//                    }
-//                }
-//                break;
-//            case 2:
-//                {
-//                    Console.Write("Введите номер задачи для удаления:");
-//                    int m = int.Parse(Console.ReadLine());
-//                    for (int i = 0; i < mas.GetLength(1); i++)
-//                    {
-//                        mas[m - 1, i] = "";
-//                    }
-//                }
-//                break;
-//            case 3:
-//                {
-//                    Console.Write("Введите номер задачи для изменения:");
-//                    int m = int.Parse(Console.ReadLine());
-//                    Console.Write("Введите название задачи:");
-//                    string name = Console.ReadLine();
-//                    Console.Write("Введите дату задачи:");
-//                    string date = Console.ReadLine();
-//                    Console.Write("Введите время задачи:");
-//                    string time = Console.ReadLine();
-//                    Console.Write("Введите приоритет задачи:");
-//                    string priority = Console.ReadLine();
-//                    mas[m, 0] = name;
-//                    mas[m, 1] = date;
-//                    mas[m, 2] = time;
-//                    mas[m, 3] = priority;
-//                }
-//                break;
-//            case 4:
-//                {
-//                    for (int i = 0; i < count; i++)
-//                    {
-//                        Console.WriteLine($"{i + 1,-3}{mas[i, 0],-20}{mas[i, 1],-10}" +
-//                            $"{mas[i, 2],-10}{mas[i, 3],5}");
-//                    }
-//                    Console.ReadKey();
-//                }
-//                break;
-//            case 5:
-//                start = false;
-//                break;
-//            default:
-//                Console.WriteLine("Введите правильное действие");
-//                Console.ReadKey();
-//                break;
-//        }
-//    }
-//    catch
-//    {
+        switch (number)
+        {
+            case 1:
+                {
+                    if (count < n)
+                    {
+                        Console.Write("Введите название задачи:");
+                        string name = Console.ReadLine();
+                        Console.Write("Введите дату задачи:");
+                        string date = Console.ReadLine();
+                        Console.Write("Введите время задачи:");
+                        string time = Console.ReadLine();
+                        Console.Write("Введите приоритет задачи:");
+                        string priority = Console.ReadLine();
+                        mas[count, 0] = name;
+                        mas[count, 1] = date;
+                        mas[count, 2] = time;
+                        mas[count, 3] = priority;
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нет свободных мест");
+                        Console.ReadKey();
+                    }
+                }
+                break;
+            case 2:
+                {
+                    Console.Write("Введите номер задачи для удаления:");
+                    int m = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < mas.GetLength(1); i++)
+                    {
+                        mas[m - 1, i] = "";
+                    }
+                }
+                break;
+            case 3:
+                {
+                    Console.Write("Введите номер задачи для изменения:");
+                    int m = int.Parse(Console.ReadLine());
+                    Console.Write("Введите название задачи:");
+                    string name = Console.ReadLine();
+                    Console.Write("Введите дату задачи:");
+                    string date = Console.ReadLine();
+                    Console.Write("Введите время задачи:");
+                    string time = Console.ReadLine();
+                    Console.Write("Введите приоритет задачи:");
+                    string priority = Console.ReadLine();
+                    mas[m, 0] = name;
+                    mas[m, 1] = date;
+                    mas[m, 2] = time;
+                    mas[m, 3] = priority;
+                }
+                break;
+            case 4:
+                {
+                    Console.Write("Введите задачу для поиска:");
+                    search = Console.ReadLine();
+                }
+                break;
+            case 5:
+                start = false;
+                break;
+            default:
+                Console.WriteLine("Введите правильное действие");
+                Console.ReadKey();
+                break;
+        }
+    }
+    catch
+    {
 
-//    }
-//}
-//while (start);
+    }
+}
+while (start);
 
 //int[,] mas = new int[9, 9];
 //for (int i = 0; i < mas.GetLength(0); i++)
@@ -111,7 +125,7 @@
 //	Console.WriteLine();
 //}
 
-int[,] mas = new int[7, 7];
+//int[,] mas = new int[7, 7];
 //for (int i = 0; i < mas.GetLength(0); i++)
 //{
 //	for (int j = 0; j < mas.GetLength(1); j++)
@@ -123,14 +137,14 @@ int[,] mas = new int[7, 7];
 //	}
 //	Console.WriteLine();
 //}
-for (int i = 0; i < mas.GetLength(0); i++)
-{
-	for (int j = 0; j < mas.GetLength(1); j++)
-	{
-		if (i == j || i + j == mas.GetLength(0) - 1) mas[i, j] = 1;
-		if (i>j&&i+j>mas.GetLength(1)-1) mas[i, j] = 1;
-		if (i<j&&i+j<mas.GetLength(1)-1) mas[i, j] = 1;
-		Console.Write(mas[i, j] + " ");
-	}
-	Console.WriteLine();
-}
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//	for (int j = 0; j < mas.GetLength(1); j++)
+//	{
+//		if (i == j || i + j == mas.GetLength(0) - 1) mas[i, j] = 1;
+//		if (i>j&&i+j>mas.GetLength(1)-1) mas[i, j] = 1;
+//		if (i<j&&i+j<mas.GetLength(1)-1) mas[i, j] = 1;
+//		Console.Write(mas[i, j] + " ");
+//	}
+//	Console.WriteLine();
+//}
---
+++ Lesson20/Program.cs
@@ -0,0 +1,62 @@
+﻿//Console.Write("Введите количество строк массива:");
+//int n = int.Parse(Console.ReadLine());
+//int[][] mas = new int[n][];
+//Random random = new Random();
+//for (int i = 0; i < mas.Length; i++)
+//{
+//	mas[i] =new int[random.Next(1, 10)];
+//}
+//for (int i = 0; i <mas.Length; i++)
+//{
+//	for (int j = 0; j < mas[i].Length; j++)
+//	{
+//		mas[i][j] = random.Next(10, 100);
+//		Console.Write(mas[i][j]+" ");
+//	}
+//	Console.WriteLine();
+//}
+//char[,] mas = new char[8, 8];
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//	for (int j = 0; j <mas.GetLength(1); j++)
+//	{
+//		if (( i + j ) % 2 == 0) mas[i, j] = 'W';
+//		else mas[i, j] = 'B';
+//		Console.Write(mas[i,j]+" ");
+//	}
+//	Console.WriteLine();
+//}
+Random random = new Random();
+string[] mast = new string[] { "Пики", "Крести", "Черви", "Буби" };
+string[] nominal = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+string[,,] players = new string[4, 2, 2];
+int[] setmast = new int[8];
+int[] setNom = new int[8];
+int count = 0;
+for (int i = 0; i < players.GetLength(0); i++)
+{
+	Console.Write(i+1+". ");
+	for (int j = 0; j < players.GetLength(1);)
+	{
+		int n = random.Next(4);
+        int m = random.Next(13);
+		bool yes = false;
+		for (int k = 0; k < 8;k++)
+		{
+			if (setmast[k] == n && setNom[k] == m)
+			{
+				yes = true;
+				break;
+            }
+		}
+		if (yes == true) continue;
+        players[i, j, 0] = mast[n];
+        players[i, j, 1] = nominal[m];
+		setmast[count] = n;
+		setNom[count] = m;
+        Console.Write(players[i, j, 1]+" " + players[i,j,0]+"\t");
+		j++;
+		count++;
+	}
+	Console.WriteLine();
+}
