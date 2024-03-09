af3aae5ee8003d0a981f172cc8be6ce503dcaaf7
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-03-09 21:22:23 +0200

    Lesson20-last

--- Lesson20/Program.cs
+++ Lesson20/Program.cs
@@ -26,37 +26,148 @@
 //	}
 //	Console.WriteLine();
 //}
+
+//Random random = new Random();
+//string[] mast = new string[] { "Пики", "Крести", "Черви", "Буби" };
+//string[] nominal = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+//string[,,] players = new string[4, 2, 2];
+//int[] setmast = new int[8];
+//int[] setNom = new int[8];
+//int count = 0;
+//for (int i = 0; i < players.GetLength(0); i++)
+//{
+//	Console.Write(i+1+". ");
+//	for (int j = 0; j < players.GetLength(1);)
+//	{
+//		int n = random.Next(4);
+//        int m = random.Next(13);
+//		bool yes = false;
+//		for (int k = 0; k < 8;k++)
+//		{
+//			if (setmast[k] == n && setNom[k] == m)
+//			{
+//				yes = true;
+//				break;
+//            }
+//		}
+//		if (yes == true) continue;
+//        players[i, j, 0] = mast[n];
+//        players[i, j, 1] = nominal[m];
+//		setmast[count] = n;
+//		setNom[count] = m;
+//        Console.Write(players[i, j, 1]+" " + players[i,j,0]+"\t");
+//		j++;
+//		count++;
+//	}
+//	Console.WriteLine();
+//}
+//for (int i = 0; i < 8; i++)
+//{
+//	Console.WriteLine(setmast[i]+" " + setNom[i]);
+//}
+
+//int[,,] mas = new int[4, 6, 8];
+//Random random = new Random();
+//for (int k = 0; k < mas.GetLength(2); k++)
+//{
+//	for (int i = 0; i < mas.GetLength(0); i++)
+//	{
+//		for (int j = 0; j < mas.GetLength(1); j++)
+//		{
+//            mas[i, j, k] = random.Next(10, 100);
+//            Console.Write(mas[i, j, k] + " ");
+//		}
+//		Console.WriteLine();
+//	}
+//	Console.WriteLine();
+//}
+//Console.Write("Введите число:");
+//int m = int.Parse(Console.ReadLine());
+//mas[3, 5, 7] = m;
+//mas[0, 0, 0] = m;
+//mas[1, 2, 3] = m;
+//for (int k = 0; k < mas.GetLength(2); k++)
+//{
+//    for (int i = 0; i < mas.GetLength(0); i++)
+//    {
+//        for (int j = 0; j < mas.GetLength(1); j++)
+//        {
+//            Console.Write(mas[i, j, k] + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//    Console.WriteLine();
+//}
+//Random random = new Random();
+//int[,] mas1 = new int[3, 3];
+//int[,] mas2 = new int[3, 3];
+//for (int i = 0; i < mas1.GetLength(0); i++)
+//{
+//	for (int j = 0; j < mas1.GetLength(1); j++)
+//	{
+//		mas1[i, j] = random.Next(10, 100);
+//		Console.Write(mas1[i,j]+" ");
+//	}
+//	Console.WriteLine();
+//}
+//Console.WriteLine();
+//for (int i = 0; i < mas2.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas2.GetLength(1); j++)
+//    {
+//        mas2[i, j] = random.Next(10, 100);
+//        Console.Write(mas2[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine();
+//int[,,] mas = new int[3, 3,2];
+//for (int k = 0; k < mas.GetLength(2); k++)
+//{
+//    for (int i = 0; i < mas.GetLength(0); i++)
+//    {
+//        for (int j = 0; j < mas.GetLength(1); j++)
+//        {
+//            if (k == 0) mas[i, j, k] = mas1[i, j];
+//            if (k == 1) mas[i, j, k] = mas2[i, j];
+//            Console.Write(mas[i,j,k] + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//    Console.WriteLine();
+//}
+
+//int[] mas1 = { 4, 8 };
+//int[] mas2 = { 16, 2 };
+//double[] masTotal1 = new double[2];
+//for (int i = 0; i < mas1.Length; i++)
+//{
+//    masTotal1[i] = ( mas1[i] + mas2[i] ) / 2;
+//    Console.Write($"{masTotal1[i]:F2} ");
+//}
+//Console.WriteLine();
+//int[] mas3 = { 1, 22 };
+//int[] mas4 = { 13, 3 };
+//double[] masTotal2 = new double[2];
+//for (int i = 0; i < mas3.Length; i++)
+//{
+//    masTotal2[i] = ( mas3[i] + mas4[i] ) / 2.0;
+//    Console.Write($"{masTotal2[i]:F2} ");
+//}
+//Console.WriteLine();
+//double[] masTotal3 = new double[2];
+
+//for (int i = 0; i < masTotal3.Length; i++)
+//{
+//    masTotal3[i] = masTotal1[i] * masTotal2[i];
+//    Console.Write($"{masTotal3[i]:F2} ");
+//}
+
+string str = "ABCDZabz019";
 Random random = new Random();
-string[] mast = new string[] { "Пики", "Крести", "Черви", "Буби" };
-string[] nominal = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
-string[,,] players = new string[4, 2, 2];
-int[] setmast = new int[8];
-int[] setNom = new int[8];
-int count = 0;
-for (int i = 0; i < players.GetLength(0); i++)
+string res = "";
+for (int i = 0; i < 4; i++)
 {
-	Console.Write(i+1+". ");
-	for (int j = 0; j < players.GetLength(1);)
-	{
-		int n = random.Next(4);
-        int m = random.Next(13);
-		bool yes = false;
-		for (int k = 0; k < 8;k++)
-		{
-			if (setmast[k] == n && setNom[k] == m)
-			{
-				yes = true;
-				break;
-            }
-		}
-		if (yes == true) continue;
-        players[i, j, 0] = mast[n];
-        players[i, j, 1] = nominal[m];
-		setmast[count] = n;
-		setNom[count] = m;
-        Console.Write(players[i, j, 1]+" " + players[i,j,0]+"\t");
-		j++;
-		count++;
-	}
-	Console.WriteLine();
+    res += str[random.Next(str.Length)];
 }
+Console.WriteLine(res);
