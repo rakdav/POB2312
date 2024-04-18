f0187fbd33776edc1401c739ceb9a1646786d60a
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-04-18 19:57:02 +0200

    DZ20-4

--- Lesson20/Program.cs
+++ Lesson20/Program.cs
@@ -171,22 +171,80 @@
 //    res += str[random.Next(str.Length)];
 //}
 //Console.WriteLine(res);
-int[,] mas =new int[4, 3];
-for (int i = 0; i < mas.GetLength(0); i++)
+//int[,] mas =new int[4, 3];
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//	for (int j = 0; j < mas.GetLength(1); j++)
+//	{
+//		mas[i, j] = int.Parse(Console.ReadLine());
+//	}
+//}
+//Console.Write("Введите число:");
+//int c = int.Parse(Console.ReadLine());
+//mas[2, 2] = c;
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas.GetLength(1); j++)
+//    {
+//        Console.Write(mas[i,j]+" "); ;
+//    }
+//    Console.WriteLine();
+//}
+Random random = new Random();
+int[,] mas1 = new int[4, 3];
+int[,] mas2 = new int[4, 3];
+for (int i = 0; i < mas1.GetLength(0); i++)
 {
-	for (int j = 0; j < mas.GetLength(1); j++)
-	{
-		mas[i, j] = int.Parse(Console.ReadLine());
-	}
-}
-Console.Write("Введите число:");
-int c = int.Parse(Console.ReadLine());
-mas[2, 2] = c;
-for (int i = 0; i < mas.GetLength(0); i++)
-{
-    for (int j = 0; j < mas.GetLength(1); j++)
+    for (int j = 0; j < mas1.GetLength(1); j++)
     {
-        Console.Write(mas[i,j]+" "); ;
+        mas1[i, j] = random.Next(-50, 51);
+        Console.Write(mas1[i,j]+" ");
     }
     Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < mas2.GetLength(0); i++)
+{
+    for (int j = 0; j < mas2.GetLength(1); j++)
+    {
+        mas2[i, j] = random.Next(-50, 51);
+        Console.Write(mas2[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[,] masSum = Sum(mas1, mas2);
+for (int i = 0; i < masSum.GetLength(0); i++)
+{
+    for (int j = 0; j < masSum.GetLength(1); j++)
+    {
+        Console.Write(masSum[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Max="+Max(masSum));
+int[,] Sum(int[,] m1, int[,] m2)
+{
+    int[,] mas = new int[m1.GetLength(0), m1.GetLength(1)];
+    for (int i = 0; i < m1.GetLength(0); i++)
+    {
+        for (int j = 0; j < m1.GetLength(1); j++)
+        {
+            mas[i,j] = m1[i, j] + m2[i, j];
+        }
+    }
+    return mas;
+}
+int Max(int[,] mas)
+{
+    int max = mas[0, 0];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            if (mas[i, j] > max) max = mas[i, j];
+        }
+    }
+    return max;
 }
