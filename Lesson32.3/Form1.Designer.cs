63c4794dcf4d8b47253bbb07dd480cf22b3882c2
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-04-25 21:06:29 +0200

    Lesson32.3Last

--- Lesson32.3/Form1.Designer.cs
+++ Lesson32.3/Form1.Designer.cs
@@ -29,98 +29,93 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            textBox1 = new TextBox();
+            tbxWidth = new TextBox();
             label2 = new Label();
-            textBox2 = new TextBox();
-            listBox1 = new ListBox();
-            pictureBox1 = new PictureBox();
-            button1 = new Button();
-            label3 = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            txbHeight = new TextBox();
+            lbxMaterial = new ListBox();
+            pbxMaterial = new PictureBox();
+            lbResult = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbxMaterial).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(35, 31);
+            label1.Location = new Point(36, 23);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(52, 15);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Ширина";
+            label1.Click += label1_Click;
             // 
-            // textBox1
+            // tbxWidth
             // 
-            textBox1.Location = new Point(100, 23);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(134, 23);
-            textBox1.TabIndex = 1;
+            tbxWidth.Location = new Point(100, 23);
+            tbxWidth.Name = "tbxWidth";
+            tbxWidth.Size = new Size(134, 23);
+            tbxWidth.TabIndex = 1;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(36, 65);
+            label2.Location = new Point(36, 73);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(47, 15);
             label2.TabIndex = 2;
-            label2.Text = "label2";
+            label2.Text = "Высота";
             // 
-            // textBox2
+            // txbHeight
             // 
-            textBox2.Location = new Point(100, 73);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(134, 23);
-            textBox2.TabIndex = 3;
+            txbHeight.Location = new Point(100, 73);
+            txbHeight.Name = "txbHeight";
+            txbHeight.Size = new Size(134, 23);
+            txbHeight.TabIndex = 3;
             // 
-            // listBox1
+            // lbxMaterial
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(34, 126);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(200, 199);
-            listBox1.TabIndex = 4;
+            lbxMaterial.FormattingEnabled = true;
+            lbxMaterial.ItemHeight = 15;
+            lbxMaterial.Items.AddRange(new object[] { "Алюминий", "Пластик", "Дерево", "Бамбук" });
+            lbxMaterial.Location = new Point(34, 126);
+            lbxMaterial.Name = "lbxMaterial";
+            lbxMaterial.Size = new Size(200, 304);
+            lbxMaterial.TabIndex = 4;
+            lbxMaterial.SelectedIndexChanged += lbxMaterial_SelectedIndexChanged;
             // 
-            // pictureBox1
+            // pbxMaterial
             // 
-            pictureBox1.Location = new Point(262, 23);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(329, 302);
-            pictureBox1.TabIndex = 5;
-            pictureBox1.TabStop = false;
+            pbxMaterial.Location = new Point(262, 23);
+            pbxMaterial.Name = "pbxMaterial";
+            pbxMaterial.Size = new Size(329, 302);
+            pbxMaterial.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbxMaterial.TabIndex = 5;
+            pbxMaterial.TabStop = false;
             // 
-            // button1
+            // lbResult
             // 
-            button1.Location = new Point(34, 347);
-            button1.Name = "button1";
-            button1.Size = new Size(190, 73);
-            button1.TabIndex = 6;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            label3.Location = new Point(267, 348);
-            label3.Name = "label3";
-            label3.Size = new Size(324, 72);
-            label3.TabIndex = 7;
-            label3.Text = "label3";
+            lbResult.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            lbResult.Location = new Point(267, 348);
+            lbResult.Name = "lbResult";
+            lbResult.Size = new Size(324, 72);
+            lbResult.TabIndex = 7;
+            lbResult.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(614, 450);
-            Controls.Add(label3);
-            Controls.Add(button1);
-            Controls.Add(pictureBox1);
-            Controls.Add(listBox1);
-            Controls.Add(textBox2);
+            Controls.Add(lbResult);
+            Controls.Add(pbxMaterial);
+            Controls.Add(lbxMaterial);
+            Controls.Add(txbHeight);
             Controls.Add(label2);
-            Controls.Add(textBox1);
+            Controls.Add(tbxWidth);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Жалюзи";
+            ((System.ComponentModel.ISupportInitialize)pbxMaterial).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -128,12 +123,11 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
+        private TextBox tbxWidth;
         private Label label2;
-        private TextBox textBox2;
-        private ListBox listBox1;
-        private PictureBox pictureBox1;
-        private Button button1;
-        private Label label3;
+        private TextBox txbHeight;
+        private ListBox lbxMaterial;
+        private PictureBox pbxMaterial;
+        private Label lbResult;
     }
 }
--- Lesson32.3/Form1.cs
+++ Lesson32.3/Form1.cs
@@ -1,10 +1,55 @@
+using System.Runtime.Versioning;
+
 namespace Lesson32._3
 {
     public partial class Form1 : Form
     {
+        private double Price;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbxMaterial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (lbxMaterial.SelectedIndex)
+                {
+                    case 0:
+                        Calculate("aluminium.jpg", 300);
+                        break;
+                    case 1:
+                        Calculate("plastik.jpg", 150);
+                        break;
+                    case 2:
+                        Calculate("wood.jpg", 120);
+                        break;
+                    case 3:
+                        Calculate("bambuk.jpg", 180);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click()
+        {
+            lbResult.Text = (double.Parse(tbxWidth.Text) * double.Parse(txbHeight.Text) * Price).ToString("c");
+        }
+        private void Calculate(string path, double price)
+        {
+            pbxMaterial.Image = Image.FromFile(path);
+            Price = price;
+            button1_Click();
         }
     }
 }
