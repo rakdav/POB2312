249603ef0d3aa5f0dca48f654e81d33c8bec18f6
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-04-27 19:32:20 +0200

    Lesson33

--- Lesson33/Program.cs
+++ Lesson33/Program.cs
@@ -100,36 +100,160 @@
 //наличия подстроки, преобразование строки в верхний/нижний регистр и
 //т.д. Напишите код, который использует эти методы для анализа и
 //обработки заданных строк
-Console.Write("Введите строку:");
-string str = Console.ReadLine();
-StringAnalizer analiz = new StringAnalizer(str);
-Console.WriteLine(analiz.getLength());
-Console.Write("Введите подстроку:");
-string sub = Console.ReadLine();
-Console.WriteLine(analiz.SubStr(sub));
-Console.WriteLine(analiz.OurToLower());
-Console.WriteLine(analiz.OurToUpper());
-class StringAnalizer
+//Console.Write("Введите строку:");
+//string str = Console.ReadLine();
+//StringAnalizer analiz = new StringAnalizer(str);
+//Console.WriteLine(analiz.getLength());
+//Console.Write("Введите подстроку:");
+//string sub = Console.ReadLine();
+//Console.WriteLine(analiz.SubStr(sub));
+//Console.WriteLine(analiz.OurToLower());
+//Console.WriteLine(analiz.OurToUpper());
+//class StringAnalizer
+//{
+//    private string Stroka;
+//    public StringAnalizer(string stroka)
+//    {
+//        Stroka = stroka;
+//    }
+//    public int getLength()
+//    {
+//        return Stroka.Length;
+//    }
+//    public bool SubStr(string substr)
+//    {
+//        return Stroka.Contains(substr);
+//    }
+//    public string OurToUpper()
+//    {
+//        return Stroka.ToUpper();
+//    }
+//    public string OurToLower()
+//    {
+//        return Stroka.ToLower();
+//    }
+//}
+
+//Тема 27. Методы. Практика
+//Задание 1.
+//Создайте класс "Массивы", содержащий методы для работы с массивами,
+//такие как нахождение суммы элементов, нахождение
+//минимального/максимального значения, сортировка массива и т.д.
+//Напишите код, который использует эти методы для обработки заданных
+//массивов.
+//Random random = new Random();
+//int[] mas = new int[random.Next(10, 20)];
+//mas = mas.Select(i => i + random.Next(10, 100)).ToArray();
+//Arrays arrays = new Arrays(mas);
+//arrays.Print();
+//Console.WriteLine("min="+arrays.Min());
+//Console.WriteLine("max="+arrays.Max());
+//arrays.Sort();
+//arrays.Print();
+//Console.WriteLine("sum="+arrays.Sum());
+//class Arrays
+//{
+//    private int[] mas;
+
+//    public Arrays(int[] mas)
+//    {
+//        this.mas = mas;
+//    }
+//    public int Sum()
+//    {
+//        //int sum = 0;
+//        //foreach (int i in mas) sum += i;
+//        //return mas;
+//        return mas.Sum();
+//    }
+//    public int Max()
+//    {
+//        //    int max = mas[0];
+//        //    for (int i = 1; i < mas.Length; i++) 
+//        //        if (mas[i] > max) max = mas[i];
+//        //    return max;
+//        return mas.Max();
+//    }
+//    public int Min()
+//    {
+//        //    int min = mas[0];
+//        //    for (int i = 1; i < mas.Length; i++) 
+//        //        if (mas[i] < min) min = mas[i];
+//        //    return min;
+//        return mas.Min();
+//    }
+//    public void Sort()
+//    {
+//        Array.Sort(mas);
+//    }
+//    public void Print()
+//    {
+//        foreach (int item in mas) Console.Write(item+" ");
+//        Console.WriteLine();
+//    }
+//}
+
+//Задание 2.
+//Создайте класс "Файловый менеджер", содержащий методы для работы с
+//файлами, такие как чтение и запись данных, проверка наличия файла,
+//удаление файла и т.д. Напишите код, который использует эти методы для
+//работы с заданными файлами.
+
+using System;
+using System.IO;
+using System.Text;
+Console.Write("Введите название файла:");
+string file = Console.ReadLine();
+string path = Environment.CurrentDirectory +@"\"+file + ".txt";
+FileManager fm = new FileManager(path);
+fm.Write("Hello");
+Console.WriteLine(fm.Read());
+
+class FileManager
 {
-    private string Stroka;
-    public StringAnalizer(string stroka)
+    private string fileName;
+
+    public FileManager(string f)
     {
-        Stroka = stroka;
+        fileName = f;
     }
-    public int getLength()
+    public bool Exists()
     {
-        return Stroka.Length;
+        FileInfo fi= new FileInfo(fileName);
+        return fi.Exists;
     }
-    public bool SubStr(string substr)
+    public async void Write(string text)
     {
-        return Stroka.Contains(substr);
+        FileInfo fi = new FileInfo(fileName);
+        if (fi.Exists)
+        {
+            using (FileStream fs=fi.OpenWrite())
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                await fs.WriteAsync(buffer,0,buffer.Length);
+            }
+        }
+        else
+        {
+            using (FileStream fs = fi.Create())
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                await fs.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
     }
-    public string OurToUpper()
+    public string Read()
     {
-        return Stroka.ToUpper();
-    }
-    public string OurToLower()
-    {
-        return Stroka.ToLower();
+        FileInfo fi = new FileInfo(fileName);
+        byte[] buffer=null;
+        if (fi.Exists)
+        {
+            using (FileStream fs = fi.OpenRead())
+            {
+                buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+            }
+        }
+        return Encoding.Default.GetString(buffer);
     }
 }
