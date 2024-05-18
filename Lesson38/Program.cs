22f5528ff96f1b5ef98266f5925cca1e9924e5dd
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-05-18 20:44:59 +0200

    Lesson38

--- Lesson38/Program.cs
+++ Lesson38/Program.cs
@@ -53,8 +53,9 @@
 //Поиск контакта по имени или номеру телефона.
 //Вывод списка всех контактов.
 
+using System;
 using System.ComponentModel;
-
+Note note = new Note();
 do
 {
     Console.Clear();
@@ -62,39 +63,106 @@ do
         "1 - Добавление нового контакта в список\n" +
         "2 - Удаление контакта по имени\n" +
         "3 - Поиск контакта по имени или номеру телефона\n" +
-        "4 - Вывод списка всех контактов\n" +
-        "5 - Выход");
+        "4 - Выход");
+    note.Print();
+    Console.Write("Введите действие:");
     int n;
     int.TryParse(Console.ReadLine(), out n);
     switch (n)
     {
         case 1: 
-            { 
-
+            {
+                Console.Write("Введите название контакта:");
+                string name = Console.ReadLine()!;
+                Console.Write("Введите номер телефона:");
+                string phone = Console.ReadLine()!;
+                Contact contact = new Contact(name, phone);
+                note.Add(contact);
             } 
             break;
-        case 2: break;
-        case 3: break;
-        case 4: break;
+        case 2: 
+            {
+                Console.WriteLine("Выберите способ:\n" +
+                    "1 - по индексу\n" +
+                    "2 - по имени\n" +
+                    "3 - по номеру телефона");
+                int m;
+                int.TryParse(Console.ReadLine(), out m);
+                switch (m)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите номер контакта:");
+                            int index;
+                            int.TryParse(Console.ReadLine(), out index);
+                            note.Remove(index);
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Введите имя контакта:");
+                            string name = Console.ReadLine()!;
+                            Contact contact = note.FindByName(name);
+                            note.Remove(contact);
+                        }
+                        break;
+                    case 3: 
+                        {
+                            Console.WriteLine("Введите телефон контакта:");
+                            string phone = Console.ReadLine()!;
+                            Contact contact = note.FindByPhone(phone);
+                            note.Remove(contact);
+                        } 
+                        break;
+                }
+               
+            }
+            break;
+        case 3: 
+            {
+                Console.WriteLine("Выберите способ:\n" +
+                   "1 - по имени\n" +
+                   "2 - по номеру телефона");
+                int m;
+                int.TryParse(Console.ReadLine(), out m);
+                switch (m)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите имя контакта:");
+                            string name = Console.ReadLine()!;
+                            Contact contact = note.FindByName(name);
+                            Console.WriteLine(contact.getName()+" "+contact.getPhone());
+                            Console.ReadKey();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Введите телефон контакта:");
+                            string phone = Console.ReadLine()!;
+                            Contact contact = note.FindByPhone(phone);
+                            Console.WriteLine(contact.getName() + " " + contact.getPhone());
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+            }
+            break;
     }
-    if (n == 5) break;
+
+    if (n == 4) break;
 }
 while (true);
 class Note
 {
     private List<Contact> contacts;
-    public Note()
+    public Note() 
     {
         contacts = new List<Contact>();
     }
-    public void Add(Contact contact)
-    {
-        contacts.Add(contact);
-    }
-    public void Remove(Contact contact)
-    {
-        contacts.Remove(contact);
-    }
+    public void Add(Contact contact)=>contacts.Add(contact);
+    public void Remove(Contact contact)=>contacts.Remove(contact);
+    public void Remove(int n) => contacts.RemoveAt(n-1);
     public Contact FindByName(string name)
     {
         foreach(Contact i in contacts)
@@ -105,6 +173,15 @@ class Note
             }
         }
         return null!;
+    }
+    public Contact FindByPhone(string phone) => contacts.FirstOrDefault(p => p.getPhone() == phone)!;
+    public void Print()
+    {
+        int i = 0;
+        foreach (Contact item in contacts)
+        {
+            Console.WriteLine(++i+"."+item.getName()+", "+item.getPhone());
+        }
     }
 }
 class Contact
